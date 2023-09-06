37c5fc51a3e344091ad7927149dd3f1670baa2ab
Piyush <piyushkumarcse44@gmail.com>
2023-09-06 20:33:09 +0530

    create urlcompressor model

--- UrlCompressor/urlcompressor/admin.py
+++ UrlCompressor/urlcompressor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UrlCompressor
+
+
 
 # Register your models here.
+
+
+admin.site.register(UrlCompressor)
+
--- UrlCompressor/urlcompressor/models.py
+++ UrlCompressor/urlcompressor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+
+
+class UrlCompressor(models.Model):
+    id = models.UUIDField(default = uuid.uuid4,unique=True,primary_key=True,editable=False)
+    OriginalUrl = models.CharField(max_length=250)
+    CompressedUrl = models.CharField(max_length=250)
+    TimeDate = models.DateTimeField(auto_created=False)
+
+    def __str__(self):
+        return self.OriginalUrl
+    
+    
 
 # Create your models here.
